29e29a9fc4c31eea4ea132f4d943961c7685ea60
Marco Rizk <pemarcorizk@gmail.com>
2019-05-01 01:08:02 +0200

    adding fast-mesa to allowed hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="huy8rUlqySmJsTH7TxpxGvFji8P5edLY3mSEjsyPDcZXuXOgLjanUcrvl3aCIl9P",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1","fast-mesa-74518.herokuapp.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
