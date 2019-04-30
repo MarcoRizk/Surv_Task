60f558ccd4313b126982d6ad411f5d5a71ce9650
Marco Rizk <pemarcorizk@gmail.com>
2019-04-30 23:47:50 +0200

    reconfigured whitnoise

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,6 +133,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 # STATIC
