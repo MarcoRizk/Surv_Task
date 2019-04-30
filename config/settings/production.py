7e1ba6de9a3d11510c5c9da4a5ecc75a8efa3eb1
Marco Rizk <pemarcorizk@gmail.com>
2019-05-01 00:27:07 +0200

    removed heroko_django

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,5 @@
 from .base import *  # noqa
 from .base import env
-import django_heroku
 
 
 # GENERAL
@@ -206,5 +205,4 @@ LOGGING = {
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-django_heroku.settings(locals())
 
